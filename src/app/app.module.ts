c49d24c5e4ff9597eb23718e22836f6b37d95531
tomaszpaww <43775829+tomaszpaww@users.noreply.github.com>
2021-06-28 21:01:18 +0200

    Add flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
     declarations: [
@@ -18,6 +19,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
         VetServiceModule,
         MatToolbarModule,
         MatSidenavModule,
+        FlexLayoutModule
     ],
     providers: [],
     bootstrap: [AppComponent]
